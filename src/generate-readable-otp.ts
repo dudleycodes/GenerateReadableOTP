1568a56aad16dfb28f5b9173c141cddc2bd39caa
James Dudley <jedudley@outlook.com>
2017-01-18 10:55:04 -0600

    Cleaned up to better follow Javascript Conventions.

--- src/generate-readable-otp.ts
+++ src/generate-readable-otp.ts
@@ -1,9 +1,9 @@
-import {subjects, verbs, objects, charPool} from './readOnlyData'
+import {subjects, verbs, objects, hexcdecimalChars} from './readOnlyData'
 
 export default class GenerateReadableOTP {
 
     /** Generate a random hexadecimal value */
-    public static Hexadecimal(length: number = 6): string[] {
+    public static hexadecimal(length: number = 6): string[] {
         if (length < 2) {
             console.warn("GenerateReadableOTP.Hexadecimal() cannot be less than 2! Value set to 2.");
             length = 2;
@@ -12,18 +12,18 @@ export default class GenerateReadableOTP {
         let returnArray: string[] = [];
 
         if (length % 2 !== 0) {
-            returnArray.push(this.GetRandomArrayValue(charPool));
+            returnArray.push(this.getRandomValueFromArray(hexcdecimalChars));
         }
 
         while (returnArray.join("").length < length) {
-            returnArray.push(this.GetRandomArrayValue(charPool) + this.GetRandomArrayValue(charPool));
+            returnArray.push(this.getRandomValueFromArray(hexcdecimalChars) + this.getRandomValueFromArray(hexcdecimalChars));
         }
 
         return returnArray;
     }
 
-    /** Generate a short phrase using words from a 4th grade spelling lists. */
-    public static ShortPhrase(maxLength: number = 24): string[] {
+    /** Generate a 3-word phrase using words from 4th grade spelling lists. */
+    public static shortPhrase(maxLength: number = 24): string[] {
         if (maxLength < 21) {
             console.warn("GenerateReadableOTP.maxLength() cannot be less than 21! Value set to 21.");
             maxLength = 21;
@@ -42,20 +42,20 @@ export default class GenerateReadableOTP {
     }
 
     /** Generates a short pharse with a random number 2-digit number either appended or prepended */
-    public static ShortPhraseNumber(): string[] {
+    public static shortPhraseNumber(): string[] {
 
-        if (this.GetRandomBoolean()) {
-            return [...this.ShortPhrase(), this.GetRandomInt(10, 99).toString()];
+        if (this.getRandomBoolean()) {
+            return [...this.shortPhrase(), this.getRandomInt(10, 99).toString()];
         } else {
-            return [this.GetRandomInt(10, 99).toString(), ...this.ShortPhrase()];
+            return [this.getRandomInt(10, 99).toString(), ...this.shortPhrase()];
         }
     }
 
-    /** Generates a random word with a trailing 2-digit number */
-    public static WordNumber(): string[] {
-        let returnArray: string[] = [this.GetRandomArrayValue([... subjects, ...objects]), this.GetRandomInt(10, 99)];
+    /** Generates a random word with a random number 2-digit number either appended or prepended */
+    public static wordNumber(): string[] {
+        let returnArray: string[] = [this.getRandomValueFromArray([... subjects, ...objects]), this.getRandomInt(10, 99)];
 
-        if (this.GetRandomBoolean()) {
+        if (this.getRandomBoolean()) {
             returnArray.reverse();
         }
 
@@ -63,17 +63,17 @@ export default class GenerateReadableOTP {
     }
 
     /** Get a random value from an array */
-    private static GetRandomArrayValue(arrayIn: any[]) {
+    private static getRandomValueFromArray(arrayIn: any[]) {
         return arrayIn[Math.floor(Math.random() * arrayIn.length)];
     }
 
     /** Get a random boolean value */
-    private static GetRandomBoolean(): boolean {
+    private static getRandomBoolean(): boolean {
         return !!(Math.random() < .5);
     }
 
     /** Get a random number between low and high */
-    private static GetRandomInt(low: number, high: number): number {
+    private static getRandomInt(low: number, high: number): number {
         return Math.floor(Math.random() * (high - low) + low);
     }
 }
